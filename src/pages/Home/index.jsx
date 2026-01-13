c29af5bcfad4e5544f72e92280592369dc20f99c
Augusto Queiroz <augusto_queiroz@id.uff.br>
2026-01-13 15:54:00 -0300

    Atualização dos sistema

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,34 +1,44 @@
-import { useEffect } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './style.css'
 import Trash from '../../assets/trash.png'
 import api from '../../services/api.js'
 
 function Home() {
-  let users = []
+  const [users, setUsers] = useState([])
+
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
 
   async function getUsers() {
-    const usersFromApi = await api.get('/users')
+    const usersFromApi = await api.get('/usuarios')
 
-    users = usersFromApi.data
-    console.log(users)
+    setUsers(usersFromApi.data)
+  }
+
+  async function createUsers(){
+    await api.post('/usuarios', {
+      name: inputName.current.value,
+      age: parseInt(inputAge.current.value),
+      email: inputEmail.current.value
+    })
   }
 
   useEffect(() => {
-    getUsers()
-  }, [])
+    getUsers()}, [])
 
   return (
     <div className='container'>
       <form>
         <h1>Cadastro de Usuários</h1>
-        <input placeholder='Nome' name='nome' type='text'/>
-        <input placeholder='Idade' name='idade' type='number'/>
-        <input placeholder='E-mail' name='email' type='email'/>
-        <button type='button'>Cadastar</button>
+        <input placeholder='Nome' name='nome' type='text' ref={inputName}/>
+        <input placeholder='Idade' name='idade' type='number' ref={inputAge}/>
+        <input placeholder='E-mail' name='email' type='email' ref={inputEmail}/>
+        <button type='button'>Cadastrar</button>
       </form>
 
   { users.map(user => (
-    <div key={user.id} className= 'card'>
+    <div key={user.id} className='card'>
       <div>
         <p>Nome: <span>{user.name}</span></p>
         <p>Idade: <span>{user.age}</span></p>
@@ -39,5 +49,8 @@ function Home() {
     </button>
    </div>
   ))}
-  </div>)}
+    </div>
+  )
+}
+
 export default Home
